4e8cc30a5371b994d41374aed40b965ea90e561d
tulasi1356 <botchatulasi1356@gmail.com>
2021-02-13 00:31:29 +0530

    login page

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb:FormBuilder,private router:Router) { }
+  loginForm = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required,Validators.minLength(8)]],
+    })
   ngOnInit(): void {
   }
+  Onsubmit() {
 
+  }
 }
