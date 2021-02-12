81db086fded077a034f7658c556916d3e20da03f
tulasi1356 <botchatulasi1356@gmail.com>
2021-02-12 19:43:27 +0530

    adding path in app.routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/',redirectTo:'register',pathMatch: 'full'},
+  {path:'/register',component:RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
